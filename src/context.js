da338e7c742982f18ccfda4d950f4399b975f489
wenhuang1 <huangwen@login.khoury.neu.edu>
2023-01-11 18:33:13 -0500

    sort tags

--- src/context.js
+++ src/context.js
@@ -1,7 +1,7 @@
 const LEETCODE_TAGS = ["-", "All", "String", "Linked List", "Map", "Tree", "Stack", "Queue", "Heap", "Graph", "Matrix", "Bit", "Randomized&Probability", "Union Find", "Monotonic Stack", "Monotonic Queue", "Trie", "Sort", "Merge Sort", "Quickselect", "Binary Search", "Topological Sort",
     "BFS", "DFS", "Minimum Spanning Tree", "Binary Search Tree", "Two Pointers", "Prefix Sum",
     "Backtracking", "Sliding Window", "Divide&Conquer", "Greedy", "DP"
-];
+].sort();
 
 const JOBS_TYPES = ["All", "Intern", "Coop", "Full Time"];
 const JOBS_TERMS = ["All", "Fall 2023", "Spring 2024", "Summer 2024"];
